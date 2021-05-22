edc3dd60aaa3d8f3ef861377b85ffdfa212d171f
tobiaditia <tobiaditia549@gmail.com>
2021-05-22 18:51:58 +0700

    routing

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    const { method } = request;
+    
+    const { method, url } = request;
 
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
- 
-    if(method === 'POST') {
-        let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    if (url === '/') {
+        if(method === 'GET') {
+            response.end('Ini adalah Homepage');
+        } else {
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+        }
+    }else if (url === '/about') {
+        if(method === 'GET') {
+            response.end('Halo! Ini adalah halaman about');
+        } else if(method === 'POST') {
+            let body = [];
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            let { name } = JSON.parse(body);
-            response.end(`Hai ${name}`);
-        })
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                let { name } = JSON.parse(body);
+                response.end(`Halo, ${name}! Ini adalah halaman about `);
+            })
+        } else {
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+        }
+    } else {
+        response.end(`Halaman tidak ditemukan !`);
     }
 
 }
