b3b72a5d7ea4212ddc6b9324b9e865008dcb61f8
tobiaditia <tobiaditia549@gmail.com>
2021-05-22 18:37:05 +0700

    body request

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+
+const requestListener = (request, response) => {
+    const { method } = request;
+
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            let { name } = JSON.parse(body);
+            response.end(`Hai ${name}`);
+        })
+    }
+
+}
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server Berjalan pada http://${host}:${port}`);
+})
